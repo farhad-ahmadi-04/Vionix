44a8b272440f6f2f9ab33d341756eb54c252b57a
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-02 13:22:06 +0330

    refactor: adjust layout structure in Header component for improved readability and responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,22 +2,28 @@ import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import Container from "./ui/container";
 import { ShoppingCart, User } from "lucide-react";
+import Navbar from "./navbar";
 
 export default function Header() {
   return (
     <header>
-      <Container className="flex justify-between items-center">
-        <div className="w-full flex gap-2">
-          <Button variant={"secondary"}>
-            <ShoppingCart />
-          </Button>
-          <Button variant={"secondary"}>
-            <User />
-          </Button>
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="w-full flex gap-2">
+            <Button variant={"secondary"}>
+              <ShoppingCart />
+            </Button>
+            <Button variant={"secondary"}>
+              <User />
+            </Button>
+          </div>
+          <h1 className="hidden md:block w-full text-center font-bold text-4xl">VIONIX</h1>
+          <div className="hidden md:block w-full text-left">
+            <Input type="search" placeholder="جستجو..." className="w-3/5" />
+          </div>
         </div>
-        <h1 className="w-full text-center font-bold text-4xl">VIONIX</h1>
-        <Input type="search" placeholder="جستجو..." className="w-full" />
+        <Navbar />
       </Container>
     </header>
   );
-};
+}
