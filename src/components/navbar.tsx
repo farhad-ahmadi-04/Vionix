f8f56140148a56feba0d8971897d931ad7a80948
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-02 13:22:50 +0330

    feat: add Navbar component for navigation

---
+++ src/components/navbar.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Button } from "./ui/button";
+import items from '../lib/navbarItems';
+import {  usePathname } from "next/navigation";
+
+
+function Navbar() {
+    // getting page url
+const activeUrl = usePathname()
+
+    return (
+        <nav className="hidden md:block my-5">
+            <ul className="flex space-x-4 justify-center">
+                {items.map((item) => (
+                    <li key={item.title}>
+                        <Button asChild variant={"link"} >
+                            <a href={item.url} className={activeUrl === item.url ? "underline" : ""}>{item.title}</a>
+                        </Button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
