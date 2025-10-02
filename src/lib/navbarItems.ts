4b624cdd8a345af741d4a31ae3b7f351066f144c
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-02 13:23:13 +0330

    feat: add navbar items for navigation links

---
+++ src/lib/navbarItems.ts
@@ -0,0 +1,32 @@
+const items = [
+  {
+    title: "خانه",
+    url: "/",
+  },
+  {
+    title: "تی شرت",
+    url: "/category/t-shirt",
+  },
+  {
+    title: "پیراهن",
+    url: "/category/shirt",
+  },
+  {
+    title: "هودی",
+    url: "/category/hoodie",
+  },
+  {
+    title: "ژاکت و پلیور",
+    url: "/category/jacket",
+  },
+  {
+    title: "درباره ما",
+    url: "/about",
+  },
+  {
+    title: "تماس با ما",
+    url: "/contact",
+  },
+]
+
+export default items;
