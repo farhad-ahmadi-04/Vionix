bcd41e3717df850e74895a0df801ccdaa686f6b0
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-04 16:17:28 +0330

    feat: add AppSidebar component for enhanced navigation and search functionality

---
+++ src/components/appSidebar.tsx
@@ -0,0 +1,42 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import items from "@/lib/navbarItems"
+import { Input } from "./ui/input"
+import { TypographyMuted } from "./typography"
+import Link from "next/link"
+
+export function AppSidebar() {
+  return (
+    <Sidebar side="right" >
+      <SidebarContent>
+        <SidebarGroup>
+        <Input placeholder="جستجو..." className="mb-4" />
+          <SidebarGroupLabel>LIONIX</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <TypographyMuted>{item.title}</TypographyMuted>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
