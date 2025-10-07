ed6ed7621a833610bd6781499d3b5553eb6a94de
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-07 20:52:11 +0330

    feat: add contact route and ContactPage component with basic structure

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/ui/container";
+
+function ContactPage() {
+    return (
+        <Container>
+            <h1>Contact Page</h1>
+        </Container>
+    );
+}
+
+export default ContactPage;
