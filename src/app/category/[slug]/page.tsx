25aaeeac52e80434c8b8583c90f2beceb4100996
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-07 20:53:04 +0330

    feat: create category route and CategoryPage component with basic structure

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/ui/container";
+
+function CategoryPage() {
+    return (
+        <Container>
+            <h1>Category Page</h1>
+        </Container>
+    );
+}
+
+export default CategoryPage;
