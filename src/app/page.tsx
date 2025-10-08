959ae4178f42d653b4447d783ddabc4c3081265d
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-08 15:33:05 +0330

    feat: update Home page to use Hero component and remove placeholder content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/hero/hero";
 import Container from "@/components/ui/container";
-import Image from "next/image";
 
 export default function Home() {
   return (
-      <Container>
-        <h1>Welcome to My Website</h1>
-        <Button>Click Me</Button>
-      </Container>
+    <Container>
+      <Hero />
+    </Container>
   );
-} 
+}
