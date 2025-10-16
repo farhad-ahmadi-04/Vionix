4ed9e26cd3bea535c334b7c8b29cd9d3e6ae4cfa
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-16 16:23:29 +0330

    refactor: update Hero component to use Link for images and improve structure

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,6 +1,6 @@
 import "@/components/hero/heroStyle.css";
 import { TypographyH3 } from "@/components/typography";
-import { HeroImage } from "@/components/hero/rightBottomHero";
+import { HeroImage } from "@/components/hero/heroImage";
 import { HeroBackground, HeroFilter } from "./heroBackground";
 import Container from "../ui/container";
 import Link from "next/link";
@@ -9,106 +9,112 @@ import { WordRotate } from "../ui/word-rotate";
 function Hero() {
   return (
     <Container className="relative">
-      <div className="flex justify-center md:-mb-7 lg:-mb-14">
-        <svg viewBox="0 0 500 250" className="w-full max-w-[500px] h-auto">
+      <div className="flex justify-center  -mb-7 lg:-mb-14">
+        <svg viewBox="0 0 500 250">
           <defs>
             <clipPath id="pathTopRight">
               <path d="M 10,20 A10,10 0,0,1 20,10 L 480,10 A10,10 0,0,1 490,20 L 490,230 A10,10 0,0,1 480,240 L 125,240 A10,10 0,0,1 115,230 L 115,160 A10,10 0,0,0 105,150 L 20,150 A10,10 0,0,1 10,140 Z" />
             </clipPath>
             <HeroFilter />
           </defs>
-          <HeroBackground clipPath="pathTopRight" />
-          <HeroImage
-            clipPath="pathTopRight"
-            image="/belondManModel.png"
-            height={300}
-          />
+          <Link href={"category/shirt"} className="group">
+            <HeroBackground clipPath="pathTopRight" />
+            <HeroImage
+              clipPath="pathTopRight"
+              image="/belondManModel.png"
+              height={300}
+            />
+          </Link>
         </svg>
-        <svg
-          viewBox="0 0 500 250"
-          className="hidden lg:block w-full max-w-[500px] h-auto"
-        >
+        <svg viewBox="0 0 500 250">
           <defs>
             <clipPath id="pathTopCenter">
               <path d="M 10,20 A10,10 0,0,1 20,10 L 480,10 A10,10 0,0,1 490,20 L 490,140 A10,10 0,0,1 480,150 L 20,150 A10,10 0,0,1 10,140 Z" />
             </clipPath>
             <HeroFilter />
           </defs>
-          <HeroBackground clipPath="pathTopCenter" />
-          <HeroImage
-            clipPath="pathTopCenter"
-            image="/manModel01.png"
-            height={350}
-          />
+          <Link href="category/t-shirt" className="group">
+            <HeroBackground clipPath="pathTopCenter" />
+            <HeroImage
+              clipPath="pathTopCenter"
+              image="/manModel01.png"
+              height={350}
+            />
+          </Link>
         </svg>
-        <Link href="category/t-shirt" className="w-full max-w-[500px] h-auto">
-          <svg viewBox="0 0 500 250">
-            <defs>
-              <clipPath id="pathTopLeft">
-                <path d="M 10,20 A10,10 0,0,1 20,10 L 480,10 A10,10 0,0,1 490,20 L 490,140 A10,10 0,0,1 480,150 L 395,150 A10,10 0,0,0 385,160 L 385,230 A10,10 0,0,1 375,240 L 20,240 A10,10 0,0,1 10,230 Z" />
-              </clipPath>
-              <HeroFilter />
-            </defs>
+        <svg viewBox="0 0 500 250" className="hidden md:block">
+          <defs>
+            <clipPath id="pathTopLeft">
+              <path d="M 10,20 A10,10 0,0,1 20,10 L 480,10 A10,10 0,0,1 490,20 L 490,140 A10,10 0,0,1 480,150 L 395,150 A10,10 0,0,0 385,160 L 385,230 A10,10 0,0,1 375,240 L 20,240 A10,10 0,0,1 10,230 Z" />
+            </clipPath>
+            <HeroFilter />
+          </defs>
+          <Link href="category/t-shirt" className="group ">
             <HeroBackground clipPath="pathTopLeft" />
             <HeroImage
               clipPath="pathTopLeft"
               image="/fullManModel.png"
               height={450}
             />
-          </svg>
-        </Link>
+          </Link>
+        </svg>
       </div>
       <div>
-        <TypographyH3 className="font-tehran text-center lg:text-6xl">
-          <WordRotate words={["بهترین باشید", "با ما", "با بهترین ها", "با", "VIONIX"]}/>        
+        <TypographyH3 className="font-tehran mr-32 sm:mr-64 md:m-0 text-center  lg:text-6xl">
+          <WordRotate
+            words={["بهترین باشید", "با ما", "با بهترین ها", "با", "VIONIX"]}
+          />
         </TypographyH3>
       </div>
       <div className="flex justify-center items-end -mt-5">
-        <svg viewBox="0 0 500 250" className="w-full max-w-[500px] h-auto">
+        <svg viewBox="0 0 500 250">
           <defs>
             <clipPath id="pathBottomRight">
               <path d="M 10,100 A10,10 0,0,1 20,90 L 105,90 A10,10 0,0,0 115,80 L 115,20 A10,10 0,0,1 125,10 L 480,10 A10,10 0,0,1 490,20 L 490,230 A10,10 0,0,1 480,240 L 20,240 A10,10 0,0,1 10,230 Z" />
             </clipPath>
             <HeroFilter />
           </defs>
-          <HeroBackground clipPath="pathBottomRight" />
-          <HeroImage
-            clipPath="pathBottomRight"
-            image="/manModel.png"
-            height={270}
-          />
+          <Link href="category/jacket" className="group">
+            <HeroBackground clipPath="pathBottomRight" />
+            <HeroImage
+              clipPath="pathBottomRight"
+              image="/manModel.png"
+              height={270}
+            />
+          </Link>
         </svg>
-        <svg
-          viewBox="0 0 500 200"
-          className="hidden lg:block w-full max-w-[500px] h-auto"
-        >
+        <svg viewBox="0 0 500 200">
           <defs>
             <clipPath id="pathBottomCenter">
               <path d="M 10,50 A10,10 0,0,1 20,40 L 480,40 A10,10 0,0,1 490,50 L 490,180 A10,10 0,0,1 480,190 L 20,190 A10,10 0,0,1 10,180 Z" />
             </clipPath>
             <HeroFilter />
           </defs>
-          <HeroBackground clipPath="pathBottomCenter" />
-          <HeroImage
-            clipPath="pathBottomCenter"
-            image="/capModel.png"
-            height={250}
-            width={300}
-          />
+          <Link href="category/cap" className="group">
+            <HeroBackground clipPath="pathBottomCenter" />
+            <HeroImage
+              clipPath="pathBottomCenter"
+              image="/capModel.png"
+              height={250}
+              width={300}
+            />
+          </Link>
         </svg>
-        <svg viewBox="0 0 500 250" className="w-full max-w-[500px] h-auto">
+        <svg viewBox="0 0 500 250" className="hidden md:block">
           <defs>
             <clipPath id="pathBottomLeft">
               <path d="M 10,20 A10,10 0,0,1 20,10 L 375,10 A10,10 0,0,1 385,20 L 385,80 A10,10 0,0,0 395,90 L 480,90 A10,10 0,0,1 490,100 L 490,230 A10,10 0,0,1 480,240 L 20,240 A10,10 0,0,1 10,230 Z" />
             </clipPath>
             <HeroFilter />
           </defs>
-          <HeroBackground clipPath="pathBottomLeft" />
-          <HeroImage
-            clipPath="pathBottomLeft"
-            image="/istockphoto-2183222014-612x612-removebg-preview.png"
-            height={266}
-          />
+          <Link href="category/hoodie" className="group">
+            <HeroBackground clipPath="pathBottomLeft" />
+            <HeroImage
+              clipPath="pathBottomLeft"
+              image="/istockphoto-2183222014-612x612-removebg-preview.png"
+              height={266}
+            />
+          </Link>
         </svg>
       </div>
     </Container>
