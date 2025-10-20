5ad1c3ad6b08d7b9ef3a68b4a60b68362a5ae3ae
Farhad Ahmadi <farhad.13ahmadi.83@gmail.com>
2025-10-20 14:30:51 +0330

    feat: create group of layout and add next image

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/hero/hero";
 import Container from "@/components/ui/container";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -17,7 +18,14 @@ export default function Home() {
             <div className="bg-amber-200 flex items-center justify-center">product03</div>
             <div className="bg-amber-200 flex items-center justify-center">product04</div>
           </div>
-          <div className="h-52 bg-amber-200 flex items-center justify-center">image</div>
+          <div className="bg-amber-200 flex items-center justify-center">
+            <Image
+              src="/model02.png"
+              alt="Model 02"
+              width={300}
+              height={350}
+            />
+          </div>
         </div>
       </Container>
     </section>
